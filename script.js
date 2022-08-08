13511612e8ab947346a5ddc67e1f0d233e551c9d
BrianASC07 <109633886+BrianASC07@users.noreply.github.com>
2022-08-08 16:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,33 +10,27 @@ const BASE_URl = "https://api.chucknorris.io/jokes/random"
 
 let requestURL = `${BASE_URl}`;
 
-function displayFact(data) {
-    let factObject = data.value;
-    console.log(factObject)
+function displayFact() {
 
-    let caption = document.querySelector("p");
-    caption.innerHTML = factObject
+    fetch(requestURL)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let factObject = data.value;
+            console.log(factObject)
+
+            let caption = document.querySelector("p");
+            caption.innerHTML = factObject
+        })
+
+        .catch(function (error) {
+            console.log("Error during fetch:", error);
+        });
+
+
 }
 
-/*
-button.onclick = function(displayFact) {
-    let factObject = data.value;
-    console.log(factObject)
 
-    let caption = document.querySelector("p");
-    caption.innerHTML = factObject
-}
-*/
-
-fetch(requestURL)
-
-    .then(function (response) {
-        return response.json();
-    })
-    .then(displayFact)
-
-    .catch(function (error) {
-        console.log("Error during fetch:", error);
-    });
-
-
+button.onclick = displayFact 
